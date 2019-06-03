d22acc8fdeb4aeb624eb1ab3c636c5715b0b23a2
Liam Chan-Wicht <liamchanwicht@hotmail.com>
2019-06-03 18:03:17 +1000

    small Variables demo showcasing destructive and relative assignments

---
+++ src/VariableDemo.java
@@ -0,0 +1,14 @@
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myAge; //declaration
+		myAge=65; //initialisation
+		System.out.println(myAge);
+		myAge=10; //destructive assignment
+		myAge=myAge+1; //relative assignment (incrementing the value by 1)
+		System.out.println(myAge);
+	}
+
+}
